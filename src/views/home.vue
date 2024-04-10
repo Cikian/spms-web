f64f2c526375dcef3ef806a27eb722f3cc296346
Cikian <cikian@outlook.com>
2024-04-10 19:43:43 +0800

    初始化

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,98 @@
 <template>
-  <div>home</div>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="">
+        <el-radio-button @click="isCollapse = !isCollapse">expand</el-radio-button>
+
+        <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            :collapse="isCollapse"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#1c2b45"
+            active-text-color="red"
+        >
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon>
+                <location/>
+              </el-icon>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group>
+              <template #title><span>Group One</span></template>
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title><span>item four</span></template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon>
+              <icon-menu/>
+            </el-icon>
+            <template #title>Navigator Two</template>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon>
+              <document/>
+            </el-icon>
+            <template #title>Navigator Three</template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon>
+              <setting/>
+            </el-icon>
+            <template #title>Navigator Four</template>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </div>
+
+
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import {ref} from 'vue'
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
 
+const isCollapse = ref(true)
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
 
-<style scoped>
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 100vh;
+}
 
+.el-menu--collapse {
+  min-height: 100vh;
+}
+
+.ddd {
+  width: 500px;
+  height: 500px;
+  background-color: aqua;
+}
 </style>
