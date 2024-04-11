a4519e27c36bd6f10bf63c9b5f3a1f8e52fd9b44
Li Tianshuo <101846203+StonePageee@users.noreply.github.com>
2024-04-11 09:15:59 +0800

    路由

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 <script setup lang="ts">
 import router from "./router";
 
-router.push("/home")
 </script>
 
 <style scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,16 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: '/',
+            name: 'index',
+            component: () => import('../views/home.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../views/Login.vue')
+        },
+        {
             path: '/home',
             name: 'home',
             component: () => import('../views/home.vue'),
